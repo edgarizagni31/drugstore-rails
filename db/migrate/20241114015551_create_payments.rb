2decd873388f710b3ebff61f0ef930fbd2851784
edgarizagni31 <ochoaedgar3108@gmail.com>
2024-11-13 21:39:51 -0500

    fix: linter errors

--- db/migrate/20241114015551_create_payments.rb
+++ db/migrate/20241114015551_create_payments.rb
@@ -1,7 +1,7 @@
 class CreatePayments < ActiveRecord::Migration[8.0]
   def change
     create_table :payments do |t|
-      t.references :cashier_id, null: false, foreign_key: {to_table: :people}
+      t.references :cashier_id, null: false, foreign_key: { to_table: :people }
       t.integer :total, null: false
       t.datetime :created_at, default: Time.now
       t.string :data
--- db/migrate/20241114020907_create_deliveries.rb
+++ db/migrate/20241114020907_create_deliveries.rb
@@ -2,8 +2,8 @@ class CreateDeliveries < ActiveRecord::Migration[8.0]
   def change
     create_table :deliveries do |t|
       t.datetime :date, default: Time.now
-      t.references :motorized, null: false, foreign_key: {to_table: :people}
-      t.references :clerk, null: false, foreign_key: {to_table: :people}
+      t.references :motorized, null: false, foreign_key: { to_table: :people }
+      t.references :clerk, null: false, foreign_key: { to_table: :people }
     end
   end
 end
